ee45449db0429177a82e99787ad4e9431474eb99
Ritu <76999781+rari1603@users.noreply.github.com>
2022-10-18 14:38:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-<!DOCTYPE html>
-<html>
-<body>
+const http = require('http');
+const port = process.env.PORT || 3000;
 
-<p>This is a paragraph.</p>
-<p>This is another paragraph.</p>
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello Node!\n'
+  res.end(msg);
+});
 
-</body>
-</html>
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
+
